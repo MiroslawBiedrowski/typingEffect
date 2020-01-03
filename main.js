96e9cd6a186732e0f81aaa70632e4f79de610075
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2020-01-03 12:53:55 +0100

    Init commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const spanText = document.querySelector(".text");
+const txt =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem omnis, laboriosam quod sapiente soluta ad repudiandae voluptas nulla veniam maxime magni voluptatem excepturi non exercitationem distinctio delectus facilis eligendi ipsam. ";
+
+const addLetter = () => {};
+
+setInterval(addLetter, 50);
